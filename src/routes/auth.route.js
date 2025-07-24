554c931c3ef9639a9ad335bfd181da2117ee6f51
Ar-06 <fabiancrack2@gmail.com>
2025-07-23 20:18:16 -0500

    change authRoute

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,6 +3,7 @@ import {
   register,
   login,
   recuperarContraseña,
+  obtenerPreguntaSeguridad,
 } from "../controllers/auth.controller.js";
 
 export const RouterAuth = Router();
@@ -10,3 +11,4 @@ export const RouterAuth = Router();
 RouterAuth.post("/register", register);
 RouterAuth.post("/login", login);
 RouterAuth.post("/recuperar", recuperarContraseña);
+RouterAuth.post("/pregunta-seguridad", obtenerPreguntaSeguridad)
